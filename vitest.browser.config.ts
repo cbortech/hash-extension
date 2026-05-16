798286eaced881ae92b79480462e988179ae74fd
Yasuhiro Kudo <ykudo@ykudo.org>
2026-05-17 00:40:32 +0900

    chore: normalize config file metadata

--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -2,7 +2,8 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
 import { playwright } from '@vitest/browser-playwright';
 
-const browser = (process.env.BROWSER as 'chromium' | 'firefox' | 'webkit') ?? 'chromium';
+const browser =
+  (process.env.BROWSER as 'chromium' | 'firefox' | 'webkit') ?? 'chromium';
 
 export default mergeConfig(
   viteConfig,
